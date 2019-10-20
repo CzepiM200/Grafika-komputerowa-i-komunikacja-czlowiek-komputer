d3cce38a6a08ae5df69b53ce7f14a3a9a9643426
CzepiMM <czepimm200@gmail.com>
2019-10-21 01:12:53 +0200

    Adding new typedef point2 and replacing functions which use two separate coordinates by function which use single float table. Adding functions which enable changing amount of levels and intensity of displacements while the program is running.

--- Labolatorium 1/Program.cpp
+++ Labolatorium 1/Program.cpp
@@ -8,13 +8,16 @@
 
 using namespace std;
 
+typedef float point2[2];
+
 int displacement;
 int depth;
+point2 startPoint = { 0, 0 };
 
 // Funkcja generuje losowy numer w zakresie od 0 do 1
 float GetRandomNumber()
 {
-	return ((float)(rand()%101)/100);
+	return ((float)(rand() % 101) / 100);
 }
 /*************************************************************************************/
 
@@ -32,54 +35,54 @@ float GetRandomDisplacement(float a)
 /*************************************************************************************/
 
 // Funkcja rysujaca kwadrat na podstawie podanego wierzchołka startowego oraz dlugosci boku
-void DrawRectangle(float x, float y, float a)
+void DrawRectangle(point2 xy, float a)
 {
 	glBegin(GL_POLYGON);
-		glColor3f(GetRandomNumber(), GetRandomNumber(), GetRandomNumber());
-		glVertex2f(x + GetRandomDisplacement(a), y + GetRandomDisplacement(a));
-		glColor3f(GetRandomNumber(), GetRandomNumber(), GetRandomNumber());
-		glVertex2f(x + a + GetRandomDisplacement(a), y + GetRandomDisplacement(a));
-		glColor3f(GetRandomNumber(), GetRandomNumber(), GetRandomNumber());
-		glVertex2f(x + a + GetRandomDisplacement(a), y - a + GetRandomDisplacement(a));
-		glColor3f(GetRandomNumber(), GetRandomNumber(), GetRandomNumber());
-		glVertex2f(x + GetRandomDisplacement(a), y - a + GetRandomDisplacement(a));
+	glColor3f(GetRandomNumber(), GetRandomNumber(), GetRandomNumber());
+	glVertex2f(xy[0] + GetRandomDisplacement(a), xy[1] + GetRandomDisplacement(a));
+	glColor3f(GetRandomNumber(), GetRandomNumber(), GetRandomNumber());
+	glVertex2f(xy[0] + a + GetRandomDisplacement(a), xy[1] + GetRandomDisplacement(a));
+	glColor3f(GetRandomNumber(), GetRandomNumber(), GetRandomNumber());
+	glVertex2f(xy[0] + a + GetRandomDisplacement(a), xy[1] - a + GetRandomDisplacement(a));
+	glColor3f(GetRandomNumber(), GetRandomNumber(), GetRandomNumber());
+	glVertex2f(xy[0] + GetRandomDisplacement(a), xy[1] - a + GetRandomDisplacement(a));
 	glEnd();
 }
 /*************************************************************************************/
 
 // Funkcja rysujaca czesc Dywanu Sierpinskiego
-void DrawPartOfSierpinskiCarpet(float x, float y, float size)
+void DrawPartOfSierpinskiCarpet(point2 xy, float size)
 {
-	DrawRectangle(x - (size / 2), y + (size / 2), size / 3);
-	DrawRectangle(x - (size / 6), y + (size / 2), size / 3);
-	DrawRectangle(x + (size / 6), y + (size / 2), size / 3);
+	DrawRectangle(point2{ xy[0] - (size / 2), xy[1] + (size / 2) }, size / 3);
+	DrawRectangle(point2{ xy[0] - (size / 6), xy[1] + (size / 2) }, size / 3);
+	DrawRectangle(point2{ xy[0] + (size / 6), xy[1] + (size / 2) }, size / 3);
 
-	DrawRectangle(x - (size / 2), y + (size / 6), size / 3);
-	DrawRectangle(x + (size / 6), y + (size / 6), size / 3);
+	DrawRectangle(point2{ xy[0] - (size / 2), xy[1] + (size / 6) }, size / 3);
+	DrawRectangle(point2{ xy[0] + (size / 6), xy[1] + (size / 6) }, size / 3);
 
-	DrawRectangle(x - (size / 2), y - (size / 6), size / 3);
-	DrawRectangle(x - (size / 6), y - (size / 6), size / 3);
-	DrawRectangle(x + (size / 6), y - (size / 6), size / 3);
+	DrawRectangle(point2{ xy[0] - (size / 2), xy[1] - (size / 6) }, size / 3);
+	DrawRectangle(point2{ xy[0] - (size / 6), xy[1] - (size / 6) }, size / 3);
+	DrawRectangle(point2{ xy[0] + (size / 6), xy[1] - (size / 6) }, size / 3);
 }
 /*************************************************************************************/
 
 // Funkcja rysujaca Dywan Sierpinskiego
-void DrawSierpinskiCarpet(float x, float y, float size, int level)
+void DrawSierpinskiCarpet(point2 xy, float size, int level)
 {
 	if (level == 1)
 	{
-		DrawPartOfSierpinskiCarpet(x, y, size);
+		DrawPartOfSierpinskiCarpet(xy, size);
 	}
 	else
 	{
-		DrawSierpinskiCarpet(x - size / 3, y + size / 3, size / 3, (level - 1));
-		DrawSierpinskiCarpet(x, y + size / 3, size / 3, (level - 1));
-		DrawSierpinskiCarpet(x + size / 3, y + size / 3, size / 3, (level - 1));
-		DrawSierpinskiCarpet(x - size / 3, y, size / 3, (level - 1));
-		DrawSierpinskiCarpet(x + size / 3, y, size / 3, (level - 1));
-		DrawSierpinskiCarpet(x - size / 3, y - size / 3, size / 3, (level - 1));
-		DrawSierpinskiCarpet(x, y - size / 3, size / 3, (level - 1));
-		DrawSierpinskiCarpet(x + size / 3, y - size / 3, size / 3, (level - 1));
+		DrawSierpinskiCarpet(point2{ xy[0] - size / 3, xy[1] + size / 3 }, size / 3, (level - 1));
+		DrawSierpinskiCarpet(point2{ xy[0], xy[1] + size / 3 }, size / 3, (level - 1));
+		DrawSierpinskiCarpet(point2{ xy[0] + size / 3, xy[1] + size / 3 }, size / 3, (level - 1));
+		DrawSierpinskiCarpet(point2{ xy[0] - size / 3, xy[1] }, size / 3, (level - 1));
+		DrawSierpinskiCarpet(point2{ xy[0] + size / 3, xy[1] }, size / 3, (level - 1));
+		DrawSierpinskiCarpet(point2{ xy[0] - size / 3, xy[1] - size / 3 }, size / 3, (level - 1));
+		DrawSierpinskiCarpet(point2{ xy[0], xy[1] - size / 3 }, size / 3, (level - 1));
+		DrawSierpinskiCarpet(point2{ xy[0] + size / 3, xy[1] - size / 3 }, size / 3, (level - 1));
 	}
 }
 /*************************************************************************************/
@@ -91,7 +94,7 @@ void RenderScene(void)
 	glClear(GL_COLOR_BUFFER_BIT);
 	// Czyszczenie okna aktualnym kolorem czyszczącym
 
-	DrawSierpinskiCarpet(0, 0, 190, depth);
+	DrawSierpinskiCarpet(startPoint, 81, depth);
 
 	glFlush();
 	// Przekazanie poleceń rysujących do wykonania
@@ -101,16 +104,16 @@ void RenderScene(void)
 // Funkcja ustalająca stan renderowania
 void MyInit(void)
 {
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-	// Kolor okna wnętrza okna - ustawiono na szary
+	glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+	// Kolor okna wnętrza okna - ustawiono na czarny
 
 	srand(time(NULL));
 	// Ustawienie punktu startowego dla mechanizmu losujacego
 }
 /*************************************************************************************/
+
 // Funkcja służąca do kontroli zachowania proporcji rysowanych obiektów
 // niezależnie od rozmiarów okna graficznego
-
 void ChangeSize(GLsizei horizontal, GLsizei vertical)
 // Parametry horizontal i vertical (szerokość i wysokość okna) są
 // przekazywane do funkcji za każdym razem, gdy zmieni się rozmiar okna
@@ -151,6 +154,43 @@ void ChangeSize(GLsizei horizontal, GLsizei vertical)
 }
 /*************************************************************************************/
 
+void KeyboardInputController(unsigned char Key, int x, int y)
+{
+	switch (Key)
+	{
+	case 'w':
+		if (depth < 7) depth++;
+		break;
+	case 's':
+		if (depth > 1) depth--;
+		break;
+	case 'a':
+		if (displacement > 0) displacement--;
+		break;
+	case 'd':
+		if (displacement < 100) displacement++;
+		break;
+	case 'j':
+		if (startPoint[0] > -10) startPoint[0]--;
+		break;
+	case 'l':
+		if (startPoint[0] < 10) startPoint[0]++;
+		break;
+	case 'i':
+		if (startPoint[1] < 10) startPoint[1]++;
+		break;
+	case 'k':
+		if (startPoint[0] > -10) startPoint[1]--;
+		break;
+
+	default:
+		break;
+	}
+
+	if (Key == 'w' || Key == 's' || Key == 'a' || Key == 'd' || Key == 'j' || Key == 'l' || Key == 'i' || Key == 'k')
+		RenderScene();
+}
+
 // Funkcja sluzaca do wprowadzania odchylenia i glebokosci
 void ImputtingDisplacementAndDepth()
 {
@@ -173,6 +213,7 @@ void main(void)
 {
 	ImputtingDisplacementAndDepth();
 
+
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
 	// Ustawienie trybu wyświetlania
 	// GLUT_SINGLE - pojedynczy bufor wyświetlania
@@ -189,6 +230,8 @@ void main(void)
 	glutReshapeFunc(ChangeSize);
 	// Dla aktualnego okna ustala funkcję zwrotną odpowiedzialną za
 	// zmiany rozmiaru okna
+
+	glutKeyboardFunc(KeyboardInputController);
 
 	MyInit();
 	// Funkcja MyInit (zdefiniowana powyżej) wykonuje wszelkie
